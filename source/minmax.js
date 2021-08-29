aff05a6f05ad4a12f10cd14c05a67a33a769a300
Maksongold <maxdudnik@mail.ru>
2021-08-29 20:38:22 +0300

    "Truncate the func"

--- source/minmax.js
+++ source/minmax.js
@@ -2,15 +2,8 @@
 
 const minmax = function (str) {
     const regexp = /(-?\.?\d+e-?\d+)|(-?\d*\.?\d+)|(-?Infinity)/g;
-    let numbsFromStr = str.match(regexp);
+    const numbsFromStr = str.match(regexp);
     if (numbsFromStr == null)
         return [undefined, undefined]
-    for (let i = 0; i < numbsFromStr.length; i++)
-        numbsFromStr[i] = -Number(numbsFromStr[i]);
-    let result = [];
-    result.push(-max(numbsFromStr));
-    for (let i = 0; i < numbsFromStr.length; i++)
-        numbsFromStr[i] = -numbsFromStr[i];
-    result.push(max(numbsFromStr));
-    return result;
+    return [Math.min(...numbsFromStr), Math.max(...numbsFromStr)];
 }
