66e61ae956a8ce1e0b4579d6fc83ad87529856b5
Maksongold <maxdudnik@mail.ru>
2021-08-30 16:40:10 +0300

    "Fixed small mistakes"

--- source/minmax.js
+++ source/minmax.js
@@ -7,11 +7,11 @@
  */
 
 const minmax = str => {
-    if (typeof(str) != "string")
+    if (typeof(str) !== 'string')
         return [undefined, undefined];
     const regexp = /(-?\.?\d+e-?\d+)|(-?\d*\.?\d+)|(-?Infinity)/g;
     const numbsFromStr = str.match(regexp);
-    if (numbsFromStr == null)
+    if (numbsFromStr === null)
         return [undefined, undefined];
     return [Math.min(...numbsFromStr), Math.max(...numbsFromStr)];
 };
